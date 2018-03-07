163a6e9a4ece5054d7ca7413f24b8592457cfbf0
Bengi Baykal <bengibaykall@gmail.com>
2018-03-07 22:18:57 +0300

    update

--- responseCodeChecker.java
+++ responseCodeChecker.java
@@ -14,39 +14,34 @@
 
 
 public class ResponseCodeCheckFromFile {
-    
     public static void main (String args[]) throws Exception
     {   
         FileResource fr = new FileResource();
         CSVParser parser = fr.getCSVParser();
         CSVRecord record = null;
         int counter = 0;
-       for(CSVRecord currentRow : parser){
+        for(CSVRecord currentRow : parser){
            
            //System.out.println(currentRow);
-        URL url = new URL((currentRow.get("url")).toString());
+            URL url = new URL((currentRow.get("url")).toString());
+            HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int code = connection.getResponseCode();
         
-        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.connect();
-
-        int code = connection.getResponseCode();
-        
-        //System.out.println("Response code of the " + url + " is : " +code);
-        if (code != 200)
-        {
+            //System.out.println("Response code of the " + url + " is : " +code);
+            if (code != 200)
+            {
             System.out.println("Reponse for the URL " + url  +":  NOK with the code " + code );
             counter++;
             
-        }
+            }
             
-       }
+        }
         
         if (counter !=0 ){
          System.out.println("Number of broken links in this file : " + counter );
         }
+        
     }
-    
-    
-    
 }
